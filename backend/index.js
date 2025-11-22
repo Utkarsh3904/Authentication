849b555e8072454ee70610b16cee2467cc921a8e
Utkarsh3904 <millenniumatom@gmail.com>
2025-11-23 01:26:49 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express'  
+import dotenv from 'dotenv'
+dotenv.config()
+
+let app = express();
+let port = process.env.PORT || 4000
+
+app.get("/", (req,res)=>{
+    res.send("hello ")
+})
+
+app.listen(port, ()=>{                                    //pass the port
+    console.log(`server is started at ${port}`)
+})
