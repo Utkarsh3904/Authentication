3298d10eb004174451c5ad4a875888ba8ada10e1
Utkarsh3904 <millenniumatom@gmail.com>
2025-12-04 23:57:01 +0530

    multer added

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -2,7 +2,14 @@ import multer from "multer"
 
 //a storehouse
 const storage = multer.diskStorage({
-    destination: (req,file,cb)=>{                            //cb is callback its a func that decide the destination 
-        cb(null, "./public")
+    destination: (req, file, cb)=>{                            //cb is callback its a func that decide the destination 
+        cb(null, "./public" )               //its the destination
+    },
+    filename: (req, file, cb)=>{
+        console.log(file);
+            
+        cb(null, file.originalname)
     }
 })
+
+export const upload = multer ({storage})  //middleware
