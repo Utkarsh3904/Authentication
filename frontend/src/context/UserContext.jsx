9f33a420612528dd3750c0a0128165db1e988b81
Utkarsh3904 <millenniumatom@gmail.com>
2025-12-08 01:24:35 +0530

    storing userdata comming form backend

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import { createContext } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import axios from 'axios'
 
 export const dataContext = createContext()
 
 function UserContext({children}) {
+
+  let [userData, setUserData] = useState(null)
+
     const serverUrl ="http://localhost:8000"
-    const value={
-      serverUrl
+
+//storing the userdata which going to come from backend 
+    const getUserdata = async()=>{
+      try {
+        let {data} = await axios.get(serverUrl + "/api/getuserdata", {withCredentials:true}) 
+        setUserData(data);  //this has to be run one time on the starting time
+      } catch (error) {
+        console.log(error);
+        
+      }
     }
+
+    const value={
+      serverUrl,userData, setUserData
+    }
+
+    useEffect(()=>{
+        getUserdata()
+    },[])
+
   return (
     <dataContext.Provider value={value}>
       {children}
