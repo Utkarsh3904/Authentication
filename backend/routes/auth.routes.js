ccf503e87e020b532208d5288e23b35a55d59766
Utkarsh3904 <millenniumatom@gmail.com>
2025-12-07 23:24:43 +0530

    get user data route setup

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express, { Router } from 'express'    //import express
-import { login, logout, signUp } from '../controllers/auth.controllers.js';
+import { getUserData, login, logout, signUp } from '../controllers/auth.controllers.js';
 import { upload } from '../middlewares/multer.js';
+import { checkAuth } from '../middlewares/checkAuth.js';
 
 
 
-const authRouter = Router();  //need only Router fn
+const authRouter = express(Router());  //need only Router fn
 
 
 // Use lowercase '/signup' so it matches the frontend POST path (/api/signup)
@@ -12,28 +13,6 @@ const authRouter = Router();  //need only Router fn
 authRouter.post("/signup", upload.single("profileImage"), signUp ) // this uplaod is a middleware used for a particular route
 authRouter.post("/login", login )
 authRouter.post("/logout", logout)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+authRouter.get("/getuserdata", checkAuth, getUserData)
 
 export default authRouter;
