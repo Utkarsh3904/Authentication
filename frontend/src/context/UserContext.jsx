bf2cd5181f7e1ab300fb148de3224e23c0a8261b
Utkarsh3904 <millenniumatom@gmail.com>
2025-12-08 17:45:55 +0530

    proper navigation

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,11 +1,13 @@
 import React, { createContext, useState, useEffect } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export const dataContext = createContext()
 
 function UserContext({children}) {
+  let navigate = useNavigate()
 
-  let [userData, setUserData] = useState({})
+  let [userData, setUserData] = useState(null)
 
     const serverUrl ="http://localhost:8000"
 
@@ -15,6 +17,7 @@ function UserContext({children}) {
         let {data} = await axios.get(serverUrl + "/api/getuserdata", {withCredentials:true}) 
         setUserData(data);  //this has to be run one time on the starting time
       } catch (error) {
+        navigate("/login")   //if try to go to home using url change then if not logged in then login will appear home will not be able to open
         console.log(error);
         
       }
